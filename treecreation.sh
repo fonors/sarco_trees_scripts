7727389d3ad8e0b5692e1945689d00f399112b5e
fonors <thea_martian@proton.me>
2024-05-18 19:42:30 +0100

    Fixed bash script not running

--- treecreation.sh
+++ treecreation.sh
@@ -1,4 +1,7 @@
-#!usr/bin/bash)
+#!/usr/bin/env bash
 echo "Enter sequence file path:"
 read filepath
-raxml-ng --all --msa $filepath --model TPM3+G+I --bs-size 1000
+outputfile = $filepath"_ALIGNED.fasta"
+
+clustalw -align -infile=$filepath -gapopen=10 -gapext=0.2 -output=PIR -outfile=$outputfile
+raxml-ng --all --msa $outputfile --model TPM3+G+I --bs-trees 1000
